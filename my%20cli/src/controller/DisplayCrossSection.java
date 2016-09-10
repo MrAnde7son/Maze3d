34b3f44ab42be2996efb4d4b868d3344dee50b58
chenerlich <chen.erlich1@gmail.com>
2016-09-10 20:15:21 +0300

    Updated classes

---
+++ my%20cli/src/controller/DisplayCrossSection.java
@@ -0,0 +1,16 @@
+package controller;
+
+/***
+ * display_by_cross_section command. Displays a 2D maze cutted by a given column.
+ * @author Itamar
+ *
+ */
+public class DisplayCrossSection implements Command {
+
+	@Override
+	public void doCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
